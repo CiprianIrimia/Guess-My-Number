2889ac85c1093a4addb4839e772d404f77cc5cac
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-10-29 13:52:38 +0300

    implemented logic game(js)

--- script.js
+++ script.js
@@ -12,16 +12,71 @@ console.log(document.querySelector('.guess').value);
 */
 
 //DEFINE THE SECRET NUMBER
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  //WHEN IS NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+
+    // WHEN PLAYER WINS
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '💥Correct Number';
+    document.querySelector('.message').textContent = '💥 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+
+    // WHEN GUESS IS TO HIGH
+  } else if (guess < 0) {
+    document.querySelector('.message').textContent =
+      '❌ Number is lower than 1';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬆ Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💤 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+    // WHEN GUESS IS TO LOW
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬇ Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💤 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start quessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+///Coding Challenge #1
+
+/*
+Implement a game reset functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
